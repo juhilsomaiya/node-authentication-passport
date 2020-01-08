b078e6762186b00e832fafd4988392241354e518
Juhil Somaiya <jgs.131296@gmail.com>
2020-01-08 15:56:08 +0530

    [IMP] code improvements

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const app = express();
 const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/myDatabase');
+mongoose.connect('mongodb://localhost/MyDatabase', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
 
 const Schema = mongoose.Schema;
 const UserDetail = new Schema({
-      username: String,
-      password: String
-    });
+  username: String,
+  password: String
+});
 const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
 const LocalStrategy = require('passport-local').Strategy;
 
@@ -18,48 +21,50 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/', (req, res) => res.sendFile('auth.html', { root : __dirname}));
+app.get('/', (req, res) => res.sendFile('auth.html', { root: __dirname }));
 
 app.get('/error', (req, res) => res.send("Invalid credentials"));
 
-app.get('/success', (req, res) => res.send("Welcome "+req.query.username+"!!"));
+app.get('/success', (req, res) => res.send("Welcome " + req.query.username + "!!"));
 
 app.post('/',
   passport.authenticate('local', { failureRedirect: '/error' }),
-  function(req, res) {
-    res.redirect('/success?username='+req.user.username);
+  function (req, res) {
+    console.log("------->>>>>", req.user.username)
+    res.redirect('/success?username=' + req.user.username);
   });
 
-passport.serializeUser(function(user, cb) {
+passport.serializeUser(function (user, cb) {
   cb(null, user.id);
 });
 
-passport.deserializeUser(function(id, cb) {
-  User.findById(id, function(err, user) {
+passport.deserializeUser(function (id, cb) {
+  User.findById(id, function (err, user) {
     cb(err, user);
   });
 });
 
 passport.use(new LocalStrategy(
-  function(username, password, done) {
-      UserDetails.findOne({
-        username: username
-      }, function(err, user) {
-        if (err) {
-          return done(err);
-        }
+  function (username, password, done) {
+    UserDetails.findOne({
+      username: username
+    }, function (err, user) {
+      console.log("------------->", username, password);
+      if (err) {
+        return done(err);
+      }
 
-        if (!user) {
-          return done(null, false);
-        }
+      if (!user) {
+        return done(null, false);
+      }
 
-        if (user.password != password) {
-          return done(null, false);
-        }
-        return done(null, user);
-      });
+      if (user.password != password) {
+        return done(null, false);
+      }
+      return done(null, user);
+    });
   }
 ));
 
 const port = process.env.PORT || 3000;
-app.listen(port , () => console.log('App listening on port ' + port));
+app.listen(port, () => console.log('App listening on port ' + port));
